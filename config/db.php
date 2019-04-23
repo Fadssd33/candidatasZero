77728f718b37cf62d6b3494afafdc6e893ab1779
leonelBusta <49352240+leonelBusta@users.noreply.github.com>
2019-04-23 13:00:20 -0700

    prueba

--- config/db.php
+++ config/db.php
@@ -1,5 +1,6 @@
 <?php 
     class Database {
+        //Prueba
         public static function connect() {
             $db = new mysqli('localhost', 'root', '', 'candidatas');
             $db->query("SET NAMES 'utf8'");
