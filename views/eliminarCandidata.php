98ee23693145dcec2e451697d614f4f7bafcf827
Fadssd33 <fadssd33@gmail.com>
2019-04-24 12:55:54 -0700

    Formato texto cambiado en eliminar candidatas

--- views/eliminarCandidata.php
+++ views/eliminarCandidata.php
@@ -44,7 +44,8 @@
                                                 <div class="description">
                                                     <h3 class="title"><?=$can->Nombre;?></h3>
                                                     <!-- Mostrar la carrera -->
-                                                    <p class="small-text"><?=$can->NombreCarrera;?></p>
+                                                    
+                                                    <p class="small-text"><?= Utils::formatText($can->NombreCarrera);?></p>
                                                     <p class="description"><?=$can->descripcion?></p>
                                                     <form action="<?=base_url?>candidata/eliminarCandidata2" method="POST">
                                                         <!-- pasa el id de la candidata -->
