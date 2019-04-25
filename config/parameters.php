d3f9f28b57035adbadcc2828c9c08ee77b8e6a4c
Fadssd33 <fadssd33@gmail.com>
2019-04-24 20:51:02 -0700

    revert to previous commit

--- config/parameters.php
+++ config/parameters.php
@@ -2,4 +2,5 @@
     define("base_url", "http://localhost/candidataszero/");
     define("controller_default", "candidataController");
     define("action_default", "index");
+    //comentario pruebahh
 ?>
